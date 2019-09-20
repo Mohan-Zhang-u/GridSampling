d073aec2bd32ca1303fffb9cc3d91457021bb531
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2019-09-19 19:10:19 -0600

    gtp commit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import numpy as np
+from utils import *
+
+functions=\
+    [
+        function_coverter(lambda x, y: x ** 2 + y ** 2, "<", 1),
+        function_coverter(lambda x, y: x, ">", 0),
+        function_coverter(lambda x, y: y, ">", 0),
+        function_coverter(lambda x, y: np.sin(x), "<", 0),
+        function_coverter(lambda x, y: x ** 2 + (y - x ** (2 / 3)) ** 2, "<", 1)
+    ]
+
+grid_density_x = grid_density_y = 0.001
+origin_x = origin_y = 0
+stopping_x = stopping_y = 0
+
+
+
+
 
