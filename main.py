26ef2cd2c3657b56240538712daeea22322dafc3
mhzhang <mhzhang@morgas-MacBook-Pro.local>
2019-09-23 13:18:44 -0400

    final

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ a = function_coverter(lambda x, y: x ** 2 + y ** 2, "<", 1)
 b = function_coverter(lambda x, y: x, ">", 0)
 c = function_coverter(lambda x, y: y, ">", 0)
 d = function_coverter(lambda x, y: np.sin(x), "<", 0)
-e = function_coverter(lambda x, y: x ** 2 + (y - np.e**((1/3) * np.log(x**2))) ** 2, "<", 1)
+e = function_coverter(lambda x, y: x ** 2 + (y - np.e**((1/3) * np.log(x**2))) ** 2, "<", 1) # heart shape
+f = function_coverter(lambda x, y: x + y, "<", 1)
+g = function_coverter(lambda x, y: x**2/3 + y**2/5, "<", 1)
+h = function_coverter(lambda x, y: (1 + 0.9 * np.cos(8 * x)) * (1 + 0.1 * np.cos(24 * x)) * (0.9 + 0.05 * np.cos(200 * x)) * (1 + np.sin(x)) + y, "<", np.pi)
+i = function_coverter(lambda x, y: (1 + 0.9 * np.cos(8 * x)) * (1 + 0.1 * np.cos(24 * x)) * (0.9 + 0.05 * np.cos(200 * x)) * (1 + np.sin(x)) - y, "<", np.pi)
+j = function_coverter(lambda x, y: np.sin(x+y) - np.cos (x*y), "<", 1)
+
 
 functions = \
     [
@@ -20,12 +26,16 @@ functions = \
 
 functions = [a]
 functions = [e]
-functions = [b,c,e]
+functions = [b,c,f]
+functions = [g]
+functions = [h]
+functions = [j]
+# functions = [b,c,e]
 
-grid_density_x = grid_density_y = 0.01
+grid_density_x = grid_density_y = 0.05 #0.01
 origin_x = origin_y = 0
 stopping_x = stopping_y = 10
-bisection_interval = (grid_density_x + grid_density_y) / 2 / 100
+bisection_interval = (grid_density_x + grid_density_y) / 2 / 50   #/100
 
 
 intersection_points = []
